8a70dd9b503d1ed73b5ab29da98b4320865dfa20
Doulsab Dodamani <doulsab.dodamani@ncs-india.com>
2024-02-15 17:38:37 +0530

    Initial commit for the project

---
+++ src/main/java/com/dd/student/project/StudentProjectApplication.java
@@ -0,0 +1,14 @@
+package com.dd.student.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentProjectApplication.class, args);
+		System.out.println("Hello this spring boot application");
+	}
+
+}
